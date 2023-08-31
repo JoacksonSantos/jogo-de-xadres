c32251ce431551b9170bafa5084371cb87202a4b
JoacksonSantos <84482420+JoacksonSantos@users.noreply.github.com>
2023-08-31 14:28:46 -0300

    cirando a classe possição

--- xadres-console/Program.cs
+++ xadres-console/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace xadres_console
+﻿using System.Security.Cryptography.X509Certificates;
+using xadres_console.Tabuleiro;
+
+namespace xadres_console
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao P;
+            P = new Posicao(3,4);
+
+            Console.WriteLine("Posição: "+P);
         }
     }
 }
